39cf2a5d1e85423c3ca8a630b5b7389556812a5f
German <thewhitehaven04@gmail.com>
2025-12-11 23:50:36 +0100

    Longest increasing subsequence

--- 300.longest-increasing-subsequence.ts
+++ 300.longest-increasing-subsequence.ts
@@ -5,8 +5,41 @@
  */
 
 // @lc code=start
-function lengthOfLIS(nums: number[]): number {
-    
-};
-// @lc code=end
+const bsLessThan = (arr: number[], value: number) => {
+  /**
+   * find the index of an element that is smaller than the value,
+   * but arr[index+1] is greater
+   */
+  let lo = 0;
+  let hi = arr.length - 1;
 
+  let index = null;
+
+  while (lo <= hi) {
+    const mid = lo + Math.floor((hi - lo) / 2);
+
+    if (arr[mid] < value) {
+      lo = mid + 1;
+    } else {
+      index = mid;
+      hi = mid - 1;
+    }
+  }
+  return index;
+};
+
+function lengthOfLIS(nums: number[]): number {
+  const accumulator = [];
+
+  for (const target of nums) {
+    const index = bsLessThan(accumulator, target);
+    if (index === null) {
+      accumulator.push(target);
+    } else {
+      accumulator[index] = target;
+    }
+  }
+
+  return accumulator.length;
+}
+// @lc code=end
